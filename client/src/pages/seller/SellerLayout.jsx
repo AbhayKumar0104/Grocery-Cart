a286101b30d70429347be4449ff6731104301506
ABHAY KUMAR <abhaybhagat2004@gmail.com>
2025-09-28 21:05:44 +0530

    Update SellerLayout.jsx

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useAppContext } from '../../context/appContext';
+import { useAppContext } from '../../context/AppContext';
 import { assets } from '../../assets/assets';
 import {Link, NavLink, Outlet } from 'react-router-dom';
 
